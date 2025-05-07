0142418cf5fbb3563090a72a71b734595e8f52bc
pablo <pablo@leadpatron.com>
2025-05-07 20:08:08 -0300

    Agrego las ventanas de Categoria, Marca y empiezo la extructura de la ventana de Articulos

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -10,11 +10,23 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApp1
 {
-    public partial class Form1 : Form
+    public partial class FrmArticulo : Form
     {
-        public Form1()
+        public FrmArticulo()
         {
             InitializeComponent();
+        }
+
+        private void msAdmiistrarCategorias_Click(object sender, EventArgs e)
+        {
+            FrmCategoria ventanaCategoria = new FrmCategoria();
+            ventanaCategoria.ShowDialog();
+        }
+
+        private void administrarMarcasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FrmMarca ventanaMarca = new FrmMarca();
+            ventanaMarca.ShowDialog();
         }
     }
 }
---
+++ WindowsFormsApp1/FrmCategoria.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class FrmCategoria
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.categoriasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.marcToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.categoriasToolStripMenuItem,
+            this.marcToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // categoriasToolStripMenuItem
+            // 
+            this.categoriasToolStripMenuItem.Name = "categoriasToolStripMenuItem";
+            this.categoriasToolStripMenuItem.Size = new System.Drawing.Size(94, 24);
+            this.categoriasToolStripMenuItem.Text = "Categorias";
+            // 
+            // marcToolStripMenuItem
+            // 
+            this.marcToolStripMenuItem.Name = "marcToolStripMenuItem";
+            this.marcToolStripMenuItem.Size = new System.Drawing.Size(64, 24);
+            this.marcToolStripMenuItem.Text = "Marca";
+            // 
+            // FrmCategoria
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "FrmCategoria";
+            this.Text = "FrmCategoria";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem categoriasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem marcToolStripMenuItem;
+    }
+}
