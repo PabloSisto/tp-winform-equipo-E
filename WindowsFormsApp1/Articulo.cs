0fac87a63a7b6a3c5ea088f26c0d13481771245a
pablo <pablo@leadpatron.com>
2025-05-06 21:21:36 -0300

    agrego clases .cs y limpio archivos con .gitignore

---
+++ WindowsFormsApp1/Articulo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    internal class Articulo
+    {
+        private int IdArticulos {  get; }
+        private string Codigo { get; set; }
+        private string Nombre { get; set; }
+        private string Descripcion { get; set; }
+        private double precio { get; set; }
+
+        private Marca marca { get; set; }
+        private Categoria categoria { get; set; }
+
+    }
+}
---
+++ WindowsFormsApp1/Marca.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    internal class Marca
+    {
+        private int IdMarca {  get; set; }
+        private string Descripcion { get; set; }
+    }
+}
